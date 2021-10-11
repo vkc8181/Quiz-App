fdb00044e2d96818617f08e81bbbb1b619575ec6
Vkc <heftyarms@gmail.com>
2021-10-11 14:14:46 +0530

    Added process.env.PORT

--- app.js
+++ app.js
@@ -195,9 +195,9 @@ app.post('/scores',(req,res)=>{
 	});
 });
 
-
-app.listen(3000,()=>{
-	console.log("App running on port 3000");
+let port=process.env.PORT||3000;
+app.listen(port,()=>{
+	console.log("App running on port "+port);
 })
 			   
 			   
